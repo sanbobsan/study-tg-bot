bc5e33e92d4079a8cd0b5338f896557c7fec6181
bob <saintsanbob@gmail.com>
2025-10-23 19:53:00 +0800

    connect database to main.py

--- bot/data_base/base.py
+++ bot/data_base/base.py
@@ -9,6 +9,6 @@ def connection(func):
     return wrapper
 
 
-async def create_table():
+async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
-from bot.handlers import router
-from config import TOKEN
+from .bot.data_base.base import create_tables
+from .bot.handlers import router
+from .config import TOKEN
+
+
+async def start_bot():
+    await create_tables()
 
 
 async def main():
     try:
-        bot = Bot(token=TOKEN)
-        dp = Dispatcher()
+        bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+        dp = Dispatcher(storage=MemoryStorage())
+
         dp.include_router(router)
+        dp.startup.register(start_bot)
+
         print("!!! Bot turned on !!!")
+        await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
+
     except Exception as e:
         print(f"!!! Error !!! \n{e}\n")
+
     finally:
+        bot.session.close()
         print("!!! Bot turned off !!!")
 
 
